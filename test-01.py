554e06ac7c6eceba5f6e44b3b1c029280e91a002
WhChess <66623661+WhChess@users.noreply.github.com>
2022-04-26 22:12:10 +0300

    Add files via upload

---
+++ test-01.py
@@ -0,0 +1,31 @@
+import chess
+import random
+import asyncio
+
+async def main():
+    board = chess.Board()
+    while True:
+        legal_moves = list(board.legal_moves)
+        a = random.choice(legal_moves)
+        await asyncio.sleep(2)
+        board.push(a)
+        print(board)
+        board = chess.Board(board.fen())
+        if board.is_checkmate():
+            print("OYUN BİTTİ!")
+            break;
+        print("========================")
+        legal_moves = list(board.legal_moves)
+        b = input("Lütfen bir hamle seçin: ")
+        while True:
+            try:
+                board.push_san(b)
+                break;
+            except:
+                b = input("Lütfen hamleyi doğru seçin: ")
+        print("========================")
+        board = chess.Board(board.fen())
+        if board.is_checkmate():
+            print("OYUN BİTTİ!")
+            break;
+asyncio.run(main())
